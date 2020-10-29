e88ebd8902037284ddc3f85635cc34bf4c97863f
Hasan71 <mahmud.cse7@gmail.com>
2020-10-30 00:39:02 +0600

    added 3 info cards

---
+++ src/components/Home/BusinessInfo/BusinessInfo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import InfoCard from '../InfoCard/InfoCard';
+
+import { faClock, faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons'
+
+const BusinessInfo = () => {
+
+    const infosData = [
+        {
+            title: 'Opening Hours',
+            description: 'We are Open 24 Hours',
+            icon: faClock,
+            background: 'primary',
+        },
+        {
+            title: 'Visit Our Location',
+            description: 'Brooklyn NY 10003 USA',
+            icon: faMapMarkerAlt,
+            background: 'dark',
+        },
+        {
+            title: 'Call Us Now',
+            description: '+1456986532',
+            icon: faPhone,
+            background: 'primary',
+        }
+    ]
+
+    return (
+        <section className="d-flex justify-content-center">
+            <div className="w-75 row">
+            {
+                infosData.map(info => <InfoCard info={info}></InfoCard> )
+            }
+            </div>
+        </section>
+    );
+};
+
+export default BusinessInfo;
